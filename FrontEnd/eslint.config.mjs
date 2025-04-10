d50f509670e68bf7e2d1d5637ad90bead8c70d14
Noure DDine <94680573+noureba@users.noreply.github.com>
2025-04-10 14:48:21 +0100

    creae responsive side bare menu for mobile

--- FrontEnd/eslint.config.mjs
+++ FrontEnd/eslint.config.mjs
@@ -3,11 +3,22 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "no-console": "warn",
+      semi: "error",
+      quotes: ["error", "double"],
+    },
+  },
   tseslint.configs.recommended,
 ]);
--- FrontEnd/src/ts/index.ts
+++ FrontEnd/src/ts/index.ts
@@ -1,2 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 require("../scss/main.scss");
 
+const barMenuBTN = document.getElementById("barMenuBTN") as HTMLButtonElement;
+const navMenu = document.getElementById("navMenu") as HTMLElement;
+
+barMenuBTN.addEventListener("click", () => {
+  navMenu.classList.toggle("activeMobileMenu");
+});
