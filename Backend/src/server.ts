a1024c955621daca4dcc79263df04d3c5f492131
Noure DDine <94680573+noureba@users.noreply.github.com>
2025-04-12 12:07:13 +0100

    create save email and send email end point

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,23 +1,53 @@
 import "dotenv/config";
 import http from "http";
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+import { Db, MongoClient } from "mongodb";
+
+//conect to db
+let db: Db | null = null;
+
+const uri = process.env.DB_URI;
+if (!uri) {
+  throw new Error("DB_URI is not defined in the environment variables");
+}
+const mongoclient = new MongoClient(uri);
+
+const conectDB = async () => {
+  try {
+    await mongoclient.connect();
+    db = mongoclient.db("WorkEaseHub");
+    console.log("Database connected successfully!");
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+      console.log(error.stack);
+    } else {
+      console.log("Unknown error", error);
+    }
+  }
+};
+
+conectDB();
 
 //transporter
 const transporter = nodemailer.createTransport({
   host: process.env.STMP_SERVER,
-  port: parseInt(process.env.STMP_PORT || "587", 10),
-  secure: process.env.STMP_SECURE === "true",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.STMP_USER,
     pass: process.env.STMP_PASS,
   },
-});
+  tls: {
+    rejectUnauthorized: false,
+  },
+} as SMTPTransport.Options);
 
 const server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
-
     //cros
-    res.setHeader("Access-Control-Allow-origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Methods", "GET, POST, PUT, DELETE");
     res.setHeader(
       "Access-Control-Allow-Headers",
@@ -40,7 +70,7 @@ const server = http.createServer(
           const mailOptions = {
             from: email,
             to: process.env.STMP_ADMIN,
-            subjuct: "hello",
+            subject: "hello",
             text: message,
           };
           await transporter.sendMail(mailOptions);
@@ -48,8 +78,57 @@ const server = http.createServer(
           res.setHeader("Content-Type", "application/json");
           res.end(JSON.stringify({ message: "done", data: parseData }));
         } catch (error) {
-          res.statusCode = 400;
-          res.end(error);
+          if (error instanceof Error) {
+            res.statusCode = 500;
+            res.end(JSON.stringify({ Error: error.message }));
+          } else {
+            res.statusCode = 500;
+            res.end("unknow error");
+          }
+        }
+      });
+    } else if (req.url === "/api/save-email" && req.method === "POST") {
+      let body = "";
+
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+
+      req.on("end", async () => {
+        try {
+          const parseData = JSON.parse(body);
+          const { email } = parseData;
+
+          if (db) {
+            const collection = db.collection("emails");
+            await collection.insertOne({ email });
+            if (!res.headersSent) {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.end(
+                JSON.stringify({
+                  success: true,
+                  message: "Email saved!",
+                })
+              );
+            }
+          } else {
+            if (!res.headersSent) {
+              res.statusCode = 500;
+              res.end(
+                JSON.stringify({
+                  success: false,
+                  message: "Failed to connect to database",
+                })
+              );
+            }
+          }
+        } catch (error) {
+          if (error instanceof Error) {
+            console.log(error);
+          } else {
+            console.log("Unkow error", error);
+          }
         }
       });
     } else {
